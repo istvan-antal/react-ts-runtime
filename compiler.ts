05413a322b9a71dd75671894cc4d8c910f820fa3
István Antal <i@istvan.xyz>
2018-08-26 14:57:26 +0100

    Use awesome-ts-loader

--- compiler.ts
+++ compiler.ts
@@ -138,21 +138,14 @@ export const createWebpackConfig = ({ hmr, development }: { hmr?: boolean; devel
         },
         module: {
             rules: [
-                /* {
-                    test: /\.tsx?$/,
-                    use: [{
-                        loader: require.resolve('ts-loader'),
-                            options: projectHasTsConfig ? undefined : {
-                                configFile: require.resolve('react-ts-runtime/tsconfig.json'),
-                                compilerOptions: {
-                                    jsx: 'react',
-                                },
-                            },
-                        },
-                    ],
-                }, */
                 // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+                {
+                    test: /\.tsx?$/,
+                    loader: "awesome-typescript-loader",
+                    options: projectHasTsConfig ? undefined : {
+                        configFileName: require.resolve('react-ts-runtime/tsconfig.json'),
+                    },
+                },
                 // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
                 { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
                 {
