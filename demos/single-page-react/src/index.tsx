20c50966dbfce638ca4258493c49c8f0f4250e2d
István Antal <i@istvan.xyz>
2018-10-22 06:53:39 +0100

    Upgrade dependencies

--- demos/single-page-react/src/index.tsx
+++ demos/single-page-react/src/index.tsx
@@ -1,4 +1,17 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import './index.scss';
 
-render(<div>Hello React</div>, document.getElementById('app'));
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="Message">
+                    Hello React
+                </div> 
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
